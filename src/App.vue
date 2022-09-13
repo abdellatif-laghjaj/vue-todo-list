1061e55a6f47924704bca6c65766908c890bcf62
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-09-13 19:27:42 +0100

    add input

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,31 @@
 <template>
-    <div>
-      Hello World
-    </div>
+  <main class="app">
+    <section class="greeting">
+      <h2 class="title">
+        Welcome, <input type="text" placeholder="Your name" v-model="name" />
+      </h2>
+    </section>
+  </main>
 </template>
 
 <script setup>
-  import { ref, onMounted, computed, watch } from 'vue'
+import { ref, onMounted, computed, watch } from 'vue'
+
+const todos = ref([])
+const name = ref('')
+const input_content = ref('')
+const input_category = ref(null)
+
+//sort todos by date
+const todos_asc = computed(() => {
+  todos.value.sort((a, b) => a.createdAt - b.createdAt)
+})
+
+watch(name, newVal => {
+  localStorage.setItem('name', newVal)
+})
+
+onMounted(() => {
+  name.value = localStorage.getItem('name') || ''
+})
 </script>
