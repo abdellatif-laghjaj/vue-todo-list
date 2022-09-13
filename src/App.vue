d8a9a0b00f5daabbd37a913e3fbef50b32edb572
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-09-13 20:16:53 +0100

    get todos and save them

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,32 @@
         Welcome, <input type="text" placeholder="Your name" v-model="name" />
       </h2>
     </section>
+
+    <section class="create-todo">
+      <h3>CREATE TODO</h3>
+      <form @submit.prevent="addTodo">
+        <h4>What are you planning to do?</h4>
+        <input type="text" placeholder="e.g Buy milk" v-model="input_content" />
+        <h4>Pick your todo category</h4>
+        <div class="options">
+          <label>
+            <input type="radio" name="category" value="business" v-model="input_category" />
+            <span class="bubble business"></span>
+            <div>Business</div>
+          </label>
+
+          <label>
+            <input type="radio" name="category" value="personal" v-model="input_category" />
+            <span class="bubble personal"></span>
+            <div>Personal</div>
+          </label>
+        </div>
+
+        <input type="submit" value="Add Todo">
+      </form>
+
+    </section>
+    {{ todos_asc }}
   </main>
 </template>
 
@@ -16,10 +42,31 @@ const name = ref('')
 const input_content = ref('')
 const input_category = ref(null)
 
+//add todo
+const addTodo = () => {
+  if(input_content.value.trim() === '' || input_category === null) {
+    return
+  }
+
+  //create todo object
+  todos.value.push({
+    content: input_content.value,
+    category: input_category.value,
+    done: false,
+    createdAt: new Date().getTime(),     
+  })
+}
+
 //sort todos by date
-const todos_asc = computed(() => {
-  todos.value.sort((a, b) => a.createdAt - b.createdAt)
+const todos_asc = computed(() => { 
+  return todos.value.sort((a, b) => b.createdAt - a.createdAt)
 })
+
+
+//save data to local storage
+watch(todos, newVal =>{
+  localStorage.setItem('todos', JSON.stringify(newVal))
+}, { deep: true })
 
 watch(name, newVal => {
   localStorage.setItem('name', newVal)
@@ -27,5 +74,6 @@ watch(name, newVal => {
 
 onMounted(() => {
   name.value = localStorage.getItem('name') || ''
+  todos.value = JSON.parse(localStorage.getItem('todos')) || []
 })
 </script>
