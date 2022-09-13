f9f621af9cf43f501ab0c4860d7fdb8ed727db27
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-09-13 20:32:08 +0100

    display todos

--- src/App.vue
+++ src/App.vue
@@ -30,7 +30,34 @@
       </form>
 
     </section>
-    {{ todos_asc }}
+
+    <section class="todo-list">
+      <div v-if="todos.length > 0">
+        <h3>Your todos</h3>
+        <div class="list">
+          <div v-for="(todo, index) in todos_asc" :key="index" :class="`todo-item ${todo.done && 'done'}`">
+            <label>
+              <input type="checkbox" v-model="todo.done" />
+              <span :class="`bubble ${todo.category}`"></span>
+            </label>
+
+            <div class="todo-content">
+              <input type="text" v-model="todo.content" />
+            </div>
+
+            <div class="actions">
+              <button class="delete" @click="deleteTodo(todo)">Delete</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div v-else>
+        <center>
+          <h3>You have no todos</h3>
+        </center>
+      </div>
+    </section>
   </main>
 </template>
 
@@ -44,7 +71,7 @@ const input_category = ref(null)
 
 //add todo
 const addTodo = () => {
-  if(input_content.value.trim() === '' || input_category === null) {
+  if (input_content.value.trim() === '' || input_category === null) {
     return
   }
 
@@ -53,18 +80,27 @@ const addTodo = () => {
     content: input_content.value,
     category: input_category.value,
     done: false,
-    createdAt: new Date().getTime(),     
+    createdAt: new Date().getTime(),
   })
+
+  //clear input
+  input_content.value = ''
+  input_category.value = null
+}
+
+//delete todo
+const deleteTodo = (todo) => {
+  return todos.value = todos.value.filter(t => t !== todo)
 }
 
 //sort todos by date
-const todos_asc = computed(() => { 
+const todos_asc = computed(() => {
   return todos.value.sort((a, b) => b.createdAt - a.createdAt)
 })
 
 
 //save data to local storage
-watch(todos, newVal =>{
+watch(todos, newVal => {
   localStorage.setItem('todos', JSON.stringify(newVal))
 }, { deep: true })
 
